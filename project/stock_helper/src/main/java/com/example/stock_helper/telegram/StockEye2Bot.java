8aa927e87b09ada16e80a77bad2100942c5e6057
YeonwooLee <fox_93@naver.com>
2023-01-24 00:34:13 +0900

    temp

--- project/stock_helper/src/main/java/com/example/stock_helper/telegram/StockEye2Bot.java
+++
@@ -1,7 +0,0 @@
-package com.example.stock_helper.telegram;
-
-
-public class StockEye2Bot {
-    public static final String BOT_TOKEN = "5963852946:AAGMkPXTP0ME4qwb_H3Rxz4UirYcNq4Vfn8";
-    public static final String BOT_USER_NAME = "moim";
-}
